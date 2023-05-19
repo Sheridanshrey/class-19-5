242ecacf2cb09d476887c163fe360e475c9cd8cb
Shrey Patel <pashreyb@sheridancollege.ca>
2023-05-19 10:30:33 -0400

    getter and setter

--- src/arraydemos23friam/Student.java
+++ src/arraydemos23friam/Student.java
@@ -6,11 +6,20 @@ public class Student {
     // I fell sleepy
     private String sId;
     private String sName;
+    private String address;
 
     public Student(String sId, String sName) {
         this.sId = sId;
         this.sName = sName;
     }   
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
     
     public String getsId() {
         return sId;
